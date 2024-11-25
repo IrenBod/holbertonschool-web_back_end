0a74dc7970e0e2c70edfb80959372973c05259d8
IrenBod <irinamora33@gmail.com>
2024-11-25 14:33:48 +0100

    1

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,14 @@
 export default function updateStudentGradeByCity(list, city, newGrades) {
-    if (!Array.isArray(list)) {
-      return [];
-    }
-    return list
-      .filter((student) => student.location === city)
-      .map((student) => {
-        const gradeObject = newGrades.find((grade) => grade.studentId === student.id);
-        return {
-          ...student,
-          grade: gradeObject ? gradeObject.grade : 'N/A',
-        };
-      });
+  if (!Array.isArray(list)) {
+    return [];
   }
+  return list
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObject = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeObject ? gradeObject.grade : 'N/A',
+      };
+    });
+}
--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,16 +1,15 @@
 export default function createInt8TypedArray(length, position, value) {
-    // Create a new ArrayBuffer with the specified length
-    const buffer = new ArrayBuffer(length);
-    const view = new DataView(buffer);
-  
-    // Check if the position is out of range
-    if (position < 0 || position >= length) {
-      throw new Error('Position outside range');
-    }
-  
-    // Set the value at the specified position
-    view.setInt8(position, value);
-  
-    return view;
+  // Create a new ArrayBuffer with the specified length
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+
+  // Check if the position is out of range
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
   }
-  
+
+  // Set the value at the specified position
+  view.setInt8(position, value);
+
+  return view;
+}
