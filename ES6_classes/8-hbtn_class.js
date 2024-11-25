53b2775c78e3dee498ff9327ad399a45fee1a4b5
IrenBod <irinamora33@gmail.com>
2024-11-25 11:11:51 +0100

    1

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -1,20 +1,20 @@
 // 8-hbtn_class.js
 
-class HolbertonClass {
-    constructor(size, location) {
-      this._size = size;       // size classe
-      this._location = location; // emplacement classe
-    }
-  
-    //numero
-    valueOf() {
-      return this._size;
-    }
-    //string
-    toString() {
-      return this._location;
-    }
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;       // size classe
+    this._location = location; // emplacement classe
   }
   
-  export default HolbertonClass;
+    //numero
+  valueOf() {
+    return this._size;
+  }
+    //string
+  toString() {
+    return this._location;
+  }
+}
+  
+
   
