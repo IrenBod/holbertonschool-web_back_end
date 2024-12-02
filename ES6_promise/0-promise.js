ac6b4c40a18f29a74a3c98a0a1885716669ea6af
IrenBod <irinamora33@gmail.com>
2024-12-02 17:03:34 +0100

    1

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,6 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // You can resolve or reject the promise here based on your condition
-    resolve('Success!');
-  });
+  return new Promise(() => {});
 }
