677b3e5c768811e790d347ae5a68fe515809e56f
IrenBod <irinamora33@gmail.com>
2024-12-07 09:18:13 +0100

    1

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+"""
+This module provides utility functions for mathematical operations.
+"""
+
 def add(a: float, b: float) -> float:
     """Returns the sum of two floats."""
     return a + b
